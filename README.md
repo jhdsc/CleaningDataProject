9acce560bbb78f2587a9b3eee32f706d72b66e65
jhdsc <jhdsc@users.noreply.github.com>
2015-10-23 18:20:12 -0400

    Original files sumbitted for the Getting and Cleaning Data Course Project

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# CleaningDataProject
-Getting and Cleaning Data Course Project
+# Getting and Cleaning Data Course Project
+
+## run_analysis.R Script Description
+
+This script can be run using the following command: source("run_analysis.R")
+
+The script requires the following files from the original Samsung data set to
+be located in the same directory as the run_analysis script:
+acitvity_labels.txt, features.txt,
+subject_test.txt, X_test.txt, y_test.txt,
+subject_train.txt, X_train.txt, and y_train.txt
+
+The script will take several seconds to run due to the time required for
+reading in the data from the above files.
+
+The result of the run_analysis.R script is a tidy data set file called 
+mean_data.txt which contains the average of the mean and standard deviation 
+measurements from the Samsung training and test data for each subject and 
+each activity. It creates this data file by performing the following functions:
+
+1. Reads in the Samsung data measurement variables from the features.txt file
+and extracts only the names and numbers corresponding to the mean and standard 
+deviation meassurements.
+
+2. Reads in the activity names from the activity_labels.txt file.
+
+3. Reads in the training data from the subject_train.txt, y_train.txt, and
+X_train.txt files into 3 separate tables and extracts only the mean and 
+standard deviation measurements (using the info obtained in Step 1 above) from 
+the third measurement table.
+
+4. Reads in the test data from the subject_test.txt, y_test.txt, and X_test.txt 
+files into 3 additional tables and extracts only the mean and standard 
+deviation measurements (using the info obtained in Step 1 above) from the 
+third additional measurement table.
+
+5. Combines the data in the tables from Steps 3 and 4 above by first merging 
+the 3 training tables into one training table and the 3 test tables into one
+test table, and then merging the training table with the test table.
+
+6. Sorts the single data table from Step 5 above by Subject and Activity
+and uses this table to compute and store in a new table the mean of each 
+measaurement variable for each subject and each activity.
+
+7. Sets the column names in the new table from Step 6 above using the info
+obtained from Steps 1 and 2, and then writes the resulting table to a file
+called mean_data.txt.
---
+++ run_analysis.R
@@ -0,0 +1,74 @@
+# This script does the following:
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 3. Uses descriptive activity names to name the activities in the data set.
+# 4. Appropriately labels the data set with descriptive variable names. 
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+# Read in the features list
+features <- read.table("./features.txt")
+# Extract the numbers corresponding to the mean and standard deviation mesaurements
+features_subset <- features[grep("mean|std",features[,2]),1]
+# Extract the names corresponding to the mean and standard deviation mesaurements
+features_subset_names <- features[grep("mean|std",features[,2]),2]
+
+# Read in the activity labels
+activity <- read.table("./activity_labels.txt")
+
+# Read in the trainig data
+train_subject <- read.table("./subject_train.txt")
+train_activity <- read.table("./y_train.txt")
+train_data <- read.table("./X_train.txt")
+# Extract only the mean and standard deviation training mesaurements
+train_subset <- train_data[,features_subset]
+# Label the columns in the training subset with the corresponding feature names
+colnames(train_subset) <- features_subset_names
+# Label the column in the training subject table
+colnames(train_subject) <- "Subject"
+# Label the column in the training activity table
+colnames(train_activity) <- "Activity"
+
+# Read in the test data
+test_subject <- read.table("./subject_test.txt")
+test_activity <- read.table("./y_test.txt")
+test_data <- read.table("./X_test.txt")
+# Extract only the mean and standard deviation test mesaurements
+test_subset <- test_data[,features_subset]
+# Label the columns in the test subset with the corresponding feature names
+colnames(test_subset) <- features_subset_names
+# Label the column in the test subject table
+colnames(test_subject) <- "Subject"
+# Label the column in the test activity table
+colnames(test_activity) <- "Activity"
+
+# Merge training subset data into one table
+train_table <- cbind(train_subject,train_activity,train_subset)
+
+# Merge test subset data into one table
+test_table <- cbind(test_subject,test_activity,test_subset)
+
+# Merge test_table and train_table into one table
+data_table <- rbind(train_table,test_table)
+
+# Sort the data table by Subject and Activity
+data_table <- data_table[order(data_table$Subject,data_table$Activity),]
+
+# Compute the average of each measurement for each activity and each subject
+mean_table <- data.frame()
+
+for (i in 1:30) {
+    for (j in 1:6) {
+        result <- apply(data_table[(data_table$Subject == i & data_table$Activity == j),3:ncol(data_table)],2,mean)
+        new_row <- c(i,j,result)
+        mean_table <- rbind(mean_table,new_row)
+    }
+}
+
+# Label the mean table columns
+names(mean_table) <- names(data_table)
+
+# Use names for the mean table Activity column values
+mean_table[,2] <- factor(mean_table[,2], labels=activity[,2])
+
+# Write the table to a file
+write.table(mean_table, file = "./mean_data.txt", row.names = FALSE)
